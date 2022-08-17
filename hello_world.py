6be6273cef5c019dcce387495c8e38383d04c41e
ethanbar11 <ethanbarbar@gmail.com>
2022-08-17 14:07:41 +0300

    bla bla

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,14 @@
 for i in range(1000):
     print(i)
 print('Bla bla bla')
+
+print('Hello World')
+print('Hello World')
+print('Hello World')
+print('Hello World')
+print('Hello World')
+
+for i in range(100):
+    print('hello')
+
+
